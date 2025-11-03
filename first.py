1550b9e5fa8b6d2b7e6b7e065eed907e7426e487
Utsab Raj Bhattarai <utsabrajbhattarai@Utsabs-MacBook-Pro.local>
2025-11-03 18:53:20 +0545

    Added the first basic part with code deduced dataframe

---
+++ first.py
@@ -0,0 +1,27 @@
+import pandas as pd
+
+# #Think of Series as a single column
+# #Think of DataFrame as a table with multiple columns (rows and columns)
+# df = pd.DataFrame([[1,2,3],[4,5,6],[7,8,9]], columns=['A', 'B', 'C'], index=['x','y','z'])
+
+# # show the first 3 rows — print so the script outputs to the console
+# print(df.head(1))
+# print(df.tail(2))
+# print(df.info())
+# print(df.nunique())
+# print(df.shape)  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#LOadinf data from CSV file
